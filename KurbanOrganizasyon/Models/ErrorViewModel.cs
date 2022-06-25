03f777d2a0ded2a1f7683d96cfdfe45a65eb7745
umpg0492 <umpg0492@B0700849>
2022-06-25 17:54:10 +0300

    Add project files.

---
+++ KurbanOrganizasyon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KurbanOrganizasyon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
